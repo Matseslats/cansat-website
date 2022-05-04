4f0a745e7c9f3a3c678236a73ed6863a0deab013
Mats <49249061+Matseslats@users.noreply.github.com>
2022-05-04 20:51:59 +0200

    Updated sponsors and google analytics
    Added four sponsors, changed the formatting to allow for more sponsors and scale better.
    
    Google Analytics should be able to differentiate between pages and tell us what pages are visited more often.
    
    Fixed some bugs where pages had the wrong title, description, links or a combination of those. Basically cleaned up the headers.
    
    Lukas must put something special in those emails.

--- sponsors.js
+++ sponsors.js
@@ -2,12 +2,20 @@ window.addEventListener("load", () => {
     // (B) GET HTML ELEMENT
     var body = document.getElementById("loadsponsors");
     body.innerHTML = `
-<AlwaysShowSection style="padding-bottom:30px;">        
+<AlwaysShowSection>        
     <div class="container reveal centerimgmobile allsponsors">
                 <a href="andoybokogpapir.html"><img src="images/andoy-bok-og-papirhandel.png" alt="Andøy Papir og Bokhandel" class="sponsor"></a>
                 <a href="jokerbleik.html"><img src="images/jokerbleik.png" alt="Joker Bleik" class="sponsor"></a>
                 <a href="polyalkemi.html"><img src="images/polyalkemi.png" alt="Poly Alkemi" class="sponsor"></a>
                 <a href="andoyenergi.html"><img src="images/andoyenergi.png" alt="Andøy Energi" class="sponsor"></a>
+    </div>
+    </AlwaysShowSection>
+    <AlwaysShowSection>   
+    <div class="container reveal centerimgmobile allsponsors">
+    <a href="hobbyking.html"><img src="images/hobbyking.png" alt="Hobbyking" class="sponsor"></a>
+    <a href="elefun.html"><img src="images/elefun.png" alt="Elefun" class="sponsor"></a>
+    <a href="4test.html"><img src="images/4test.png" alt="4Test" class="sponsor"></a>
+    <a href="designfabrikken.html"><img src="images/designfabrikken.png" alt="Designfabrikken" class="sponsor"></a>
     </div>
 </AlwaysShowSection>
     `;
